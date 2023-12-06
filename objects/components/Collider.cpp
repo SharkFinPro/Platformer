b13e564b58dbbf02353ea99c130061a07ecbb63c
Alex Martin <sharkfinpro@hotmail.com>
2023-12-06 07:40:11 -0800

    Update Collider.cpp

--- objects/components/Collider.cpp
+++ objects/components/Collider.cpp
@@ -9,15 +9,10 @@ Collider::Collider()
 bool Collider::collidesWith(GameObject* other)
 {
   auto transform = dynamic_cast<Transform*>(owner->getComponent(ComponentType::transform));
-  if (!transform)
-    return false;
-
   auto otherTransform = dynamic_cast<Transform*>(other->getComponent(ComponentType::transform));
-  if (!otherTransform)
-    return false;
-
   auto otherCollider = dynamic_cast<Collider*>(other->getComponent(ComponentType::collider));
-  if (!otherCollider)
+  
+  if (!transform || !otherTransform || !otherCollider)
     return false;
 
   std::vector<Vec3<float>> simplex;
